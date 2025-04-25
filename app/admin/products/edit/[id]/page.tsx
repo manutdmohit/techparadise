bdd447034369abec5ebd2b62a0f9293173e633b4
Mohit Saud <mohit@prabidhilabs.com>
2025-04-25 14:17:42 +0545

    refactor: define interface for EditProductPage props

--- app/admin/products/edit/[id]/page.tsx
+++ app/admin/products/edit/[id]/page.tsx
@@ -1,6 +1,10 @@
 import ProductForm from '../../product-form';
 
-export default function EditProductPage(params: { id: string }) {
+interface EditProductPageProps {
+  params: { id: string };
+}
+
+export default function EditProductPage({ params }: EditProductPageProps) {
   return (
     <div className="space-y-6">
       <h1 className="text-3xl font-bold tracking-tight text-white">
